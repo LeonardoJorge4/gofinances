27c98e7f0a83258f8992174127ee538cc842f185
leonardo <leonardoti4437@gmail.com>
2022-02-15 23:01:47 -0300

    implementing highlight card with styles and scroll horizontal, align header to top

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { HightlightCard } from '../../components/HighlightCard';
 
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HightlightCards
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'http://github.com/LeonardoJorge4.png' }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Leonardo</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HightlightCards>
+        <HightlightCard />
+        <HightlightCard />
+        <HightlightCard />
+      </HightlightCards>
     </Container>
   )
 }
