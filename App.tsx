c09a7d77079da3380e241511269ae6330d1127e6
leonardo <leonardoti4437@gmail.com>
2022-04-01 08:49:17 -0300

    commit

--- App.tsx
+++ App.tsx
@@ -9,9 +9,8 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins'
 
-import { Dashboard } from './src/screens/Dashboard';
-
 import theme from './src/global/styles/theme';
+import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +25,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme} >
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
